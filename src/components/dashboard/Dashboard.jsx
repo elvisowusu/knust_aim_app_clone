22fdf4391a3585cb6b747acf2416a8e3358a9a2f
elvisowusu <owusuelvisgyasi@gmail.com>
2024-10-24 03:30:55 +0000

    almost done with navbar for mobile view

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import { DashBody } from "./DashBody";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
+import { NavBar } from "./NavBar";
 
 export const Dashboard = () => {
   return (
     <div className="">
+      <NavBar/>
       <Header />
       <DashBody />
       <Footer />
