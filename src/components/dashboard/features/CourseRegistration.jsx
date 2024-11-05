99df331479e124886d83e89725203533e059a90e
elvisowusu <owusuelvisgyasi@gmail.com>
2024-11-05 19:27:04 +0000

    pause

--- src/components/dashboard/features/CourseRegistration.jsx
+++ src/components/dashboard/features/CourseRegistration.jsx
@@ -1,32 +1,52 @@
 import { CourseRegistrationItems } from "../../../utils/CourseRegistrationItems";
 import { FiCheckCircle } from "react-icons/fi";
-import { Btn, Heading } from "../../../utils/Layout";
+import { Btn, Heading, Layout } from "../../../utils/Layout";
+
+// export const CourseRegistration = () => {
+//   return (
+//     <div className="pb-[1.2rem] bg-white md:w-[83%] rounded-[0.3rem] shadow-sm flex flex-col items-center">
+//       <Heading headText={"REGISTRATION INSTRUCTIONS"} />
+//       <section className=" border-[#d9f1e1] mt-5 px-6">
+//         <h1 className="mb-2 text-red-500">
+//           {CourseRegistrationItems[0].heading}
+//         </h1>
+//         <p className="mb-[0.5rem]">{CourseRegistrationItems[0].passage}</p>
+//         {CourseRegistrationItems.slice(1).map(({ heading, passage }, index) => (
+//           <div key={index} className="flex gap-4">
+//             <FiCheckCircle className="shrink-0 mt-[0.4rem] text-[#5ab887] rounded-full bg-[#d9f7e7]" />
+//             <div className="">
+//               <h3 className="text-[#13c56b]">{heading}</h3>
+//               <p className="mt-[0.6rem] mb-[0.6rem]">{passage}</p>
+//             </div>
+//           </div>
+//         ))}
+//       </section>
+//       <div className="flex flex-col sm:flex-row lg:flex-row gap-[0.2rem] justify-center  w-full items-center h-[4rem] text-center">
+//         <Btn />
+//         <button className="border bg-[#13c56b] text-white hover:text-white hover:bg-[#10a75b] hover:border-[#0f9e56] active:bg-[#0f9e56] ease-out transition-all 3s w-fit text-sm py-[0.55rem] px-3 rounded-[0.3rem] border-[#13c56b] flex justify-center items-center">
+//           Proceed to Course Registration
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
 
 export const CourseRegistration = () => {
   return (
-    <div className="pb-[1.2rem] bg-white md:w-[83%] rounded-[0.3rem] shadow-sm flex flex-col items-center">
-      <Heading headText={"REGISTRATION INSTRUCTIONS"} />
-      <section className=" border-[#d9f1e1] mt-5 px-6">
-        <h1 className="mb-2 text-red-500">
-          {CourseRegistrationItems[0].heading}
-        </h1>
-        <p className="mb-[0.5rem]">{CourseRegistrationItems[0].passage}</p>
-        {CourseRegistrationItems.slice(1).map(({ heading, passage }, index) => (
-          <div key={index} className="flex gap-4">
-            <FiCheckCircle className="shrink-0 mt-[0.4rem] text-[#5ab887] rounded-full bg-[#d9f7e7]" />
-            <div className="">
-              <h3 className="text-[#13c56b]">{heading}</h3>
-              <p className="mt-[0.6rem] mb-[0.6rem]">{passage}</p>
-            </div>
+    <Layout mainHeadText={"REGISTRATION INSTRUCTIONS"} section={<section className=" border-[#d9f1e1] mt-5 px-6">
+      <h1 className="mb-2 text-red-500">
+        {CourseRegistrationItems[0].heading}
+      </h1>
+      <p className="mb-[0.5rem]">{CourseRegistrationItems[0].passage}</p>
+      {CourseRegistrationItems.slice(1).map(({ heading, passage }, index) => (
+        <div key={index} className="flex gap-4">
+          <FiCheckCircle className="shrink-0 mt-[0.4rem] text-[#5ab887] rounded-full bg-[#d9f7e7]" />
+          <div className="">
+            <h3 className="text-[#13c56b]">{heading}</h3>
+            <p className="mt-[0.6rem] mb-[0.6rem]">{passage}</p>
           </div>
-        ))}
-      </section>
-      <div className="flex flex-col sm:flex-row lg:flex-row gap-[0.2rem] justify-center  w-full items-center h-[4rem] text-center">
-        <Btn />
-        <button className="border bg-[#13c56b] text-white hover:text-white hover:bg-[#10a75b] hover:border-[#0f9e56] active:bg-[#0f9e56] ease-out transition-all 3s w-fit text-sm py-[0.55rem] px-3 rounded-[0.3rem] border-[#13c56b] flex justify-center items-center">
-          Proceed to Course Registration
-        </button>
-      </div>
-    </div>
+        </div>
+      ))}
+    </section>}/>
   );
 };
--- src/utils/Layout.jsx
+++ src/utils/Layout.jsx
@@ -22,28 +22,32 @@ export const Heading = ({ headText }) => {
   );
 };
 
+
+export const Layout = ({ mainHeadText, mainBtn, section }) => {
+    return (
+    <div className="pb-[1.2rem] bg-white md:w-[83%] rounded-[0.3rem] shadow-sm flex flex-col items-center">
+      <section className="flex flex-col border-b-2 border-[#d9f1e1] rounded-b-[13rem_15%] justify-center items-center bg-[#D9F1E1] w-full h-[8.7rem] sm:h-[7.8rem]">
+        <h1 className="text-[#504e51] text-center leading-[1.6rem] text-[1.4rem] md:text-[1.6rem] px-6">
+          {mainHeadText}
+        </h1>
+      </section>
+      <hr className="bg-[#d9f1e1] border-none h-[0.2rem] sm:h-[0.04rem] md:h-[0.01rem] mt-[0.2rem] w-full " />
+        <>
+          {section}
+        </>
+        <div className="flex flex-col sm:flex-row lg:flex-row gap-[0.2rem] justify-center  w-full items-center h-[4rem] text-center">
+        <button className="border text-red-400 hover:text-white hover:bg-red-400 hover:border-none ease-out transition-all 3s w-fit text-sm py-[0.55rem] px-3 rounded-[0.3rem] border-red-400 flex justify-center items-center">
+          <FaArrowLeftLong size={11} />
+          Back to Main Menu
+        </button>
+        <button className="border bg-[#13c56b] text-white hover:text-white hover:bg-[#10a75b] hover:border-[#0f9e56] active:bg-[#0f9e56] ease-out transition-all 3s w-fit text-sm py-[0.55rem] px-3 rounded-[0.3rem] border-[#13c56b] flex justify-center items-center">
+          {mainBtn}
+        </button>
+      </div>
+    </div>
+  );
+};
+
 Heading.propTypes = {
   headText: PropTypes.string.isRequired,
 };
-
-// export const Layout = ({ mainHeadText, mainBtn }) => {
-//   return (
-//     <div className="pb-[1.2rem] bg-white md:w-[83%] rounded-[0.3rem] shadow-sm flex flex-col items-center">
-//       <section className="flex flex-col border-b-2 border-[#d9f1e1] rounded-b-[13rem_15%] justify-center items-center bg-[#D9F1E1] w-full h-[8.7rem] sm:h-[7.8rem]">
-//         <h1 className="text-[#504e51] text-center leading-[1.6rem] text-[1.4rem] md:text-[1.6rem] px-6">
-//           {mainHeadText}
-//         </h1>
-//       </section>
-//       <hr className="bg-[#d9f1e1] border-none h-[0.2rem] sm:h-[0.04rem] md:h-[0.01rem] mt-[0.2rem] w-full " />
-//       <div className="flex flex-col sm:flex-row lg:flex-row gap-[0.2rem] justify-center  w-full items-center h-[4rem] text-center">
-//         <button className="border text-red-400 hover:text-white hover:bg-red-400 hover:border-none ease-out transition-all 3s w-fit text-sm py-[0.55rem] px-3 rounded-[0.3rem] border-red-400 flex justify-center items-center">
-//           <FaArrowLeftLong size={11} />
-//           Back to Main Menu
-//         </button>
-//         <button className="border bg-[#13c56b] text-white hover:text-white hover:bg-[#10a75b] hover:border-[#0f9e56] active:bg-[#0f9e56] ease-out transition-all 3s w-fit text-sm py-[0.55rem] px-3 rounded-[0.3rem] border-[#13c56b] flex justify-center items-center">
-//           {mainBtn}
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
