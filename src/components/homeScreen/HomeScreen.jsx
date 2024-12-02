083eeae9980a414184173da4cf630a2f85f45b93
elvisowusu <owusuelvisgyasi@gmail.com>
2024-12-02 11:56:27 +0000

    more adjustments

--- src/components/homeScreen/HomeScreen.jsx
+++ src/components/homeScreen/HomeScreen.jsx
@@ -5,10 +5,10 @@ const HomeScreen = () => {
   return (
     <div className="font-Rubik px-3 flex flex-col justify-center items-center bg-gradient-with-cover">
       <div className="absolute w-full left-0 top-0 bg-cover-pattern bg-cover bg-center h-screen"></div>
-      <section className="z-10 mt-12 w-full sm:w-[92%] lg:w-[87%] xl:w-[75%] lg:flex lg:flex-row lg:justify-between lg:align-middle text-center rounded-[0.2rem] mb-[5.5rem] lg:mt-[6rem] bg-white  shadow-slate-300 shadow-sm">
+      <section className="z-10 mt-12 w-full sm:w-[92%] md:w-[80%] lg:w-[87%] xl:w-[75%] lg:flex lg:flex-row lg:justify-between lg:align-middle text-center rounded-[0.2rem] mb-[5.5rem] lg:mt-[6rem] bg-white  shadow-slate-300 shadow-sm">
         <div className="max-h-full min-h-[22rem]  lg:w-[50%]  flex flex-col items-center lg:justify-center rounded-t-[0.2rem] bg-aimLightGreen">
           <img
-            className=" h-[9.8rem] sm:h-[14.5rem] md:h-[20rem] lg:h-[12rem] xl:h-[15.5rem] mt-[1.5rem]"
+            className=" h-[9.8rem] sm:h-[14.5rem] md:h-[15rem] lg:h-[12rem] xl:h-[15.5rem] mt-[1.5rem]"
             src={aimImg}
             alt="aim img"
           />
