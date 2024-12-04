b810c5f477bfb43c08abbbdea46de2af76374dbd
elvisowusu <owusuelvisgyasi@gmail.com>
2024-12-04 15:13:04 +0000

    update

--- src/components/dashboard/features/MainDashBoard.jsx
+++ src/components/dashboard/features/MainDashBoard.jsx
@@ -34,7 +34,7 @@ const MainDashBoard = () => {
   };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full p-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full p-4">
       {DashboardItems.map((item, index) => (
         <div
           key={index}
